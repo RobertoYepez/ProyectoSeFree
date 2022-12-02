0a34a2662cb5beeb0b65e6bf051829c86915d294
lyepezc <robertoyepezrivera@gmail.com>
2022-12-01 22:34:48 -0500

    hi

--- app/src/main/java/com/example/proyectosefree/ActivityMovimientos.kt
+++ app/src/main/java/com/example/proyectosefree/ActivityMovimientos.kt
@@ -1,15 +1,59 @@
 package com.example.proyectosefree
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Build
 import android.os.Bundle
-import com.example.proyectosefree.databinding.ActivityMainBinding
+import android.view.View
+import android.widget.DatePicker
+import android.widget.DatePicker.OnDateChangedListener
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.proyectosefree.databinding.ActivityMovimientosBinding
 
+
 class ActivityMovimientos : AppCompatActivity() {
-    lateinit var binding : ActivityMovimientosBinding
+    lateinit var binding: ActivityMovimientosBinding
+
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMovimientosBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var etDates = binding.etdate
+        var dpDates = binding.dpFecha
+
+
+
+        etDates.setText(getDatePicker())
+
+
+
+        dpDates.setOnDateChangedListener { dpDates, year, month, day ->
+            etDates.setText(getDatePicker())
+            dpDates.visibility = View.GONE
+
+
+        }
+
     }
+
+    fun getDatePicker(): String {
+
+        var day = binding.dpFecha .dayOfMonth.toString().padStart(2, '0')
+        var month = (binding.dpFecha.month + 1).toString().padStart(2, '0')
+        var year = binding.dpFecha.year.toString().padStart(4, '0')
+        return day + "/" + month + "/" + year
+    }
+
+    fun showCalendary(view: View) {
+
+        binding.dpFecha.visibility = View.VISIBLE
+    }
+
 }
+
+
--- app/src/main/java/com/example/proyectosefree/MainActivity.kt
+++ app/src/main/java/com/example/proyectosefree/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.proyectosefree
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.proyectosefree.databinding.ActivityMainBinding
@@ -10,5 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bttonRegsitrarme.setOnClickListener {
+
+            val miIntent = Intent(this,ActivityMovimientos::class.java)
+            startActivity(miIntent)
+
+        }
     }
 }
