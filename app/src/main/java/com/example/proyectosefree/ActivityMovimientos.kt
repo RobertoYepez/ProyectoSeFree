254e07c11422f6e0d7eb2d420f724016f8c187e1
lyepezc <robertoyepezrivera@gmail.com>
2022-12-06 09:47:00 -0500

    logre hacer el datapicker

--- app/src/main/java/com/example/proyectosefree/ActivityMovimientos.kt
+++ app/src/main/java/com/example/proyectosefree/ActivityMovimientos.kt
@@ -10,21 +10,45 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyectosefree.databinding.ActivityMovimientosBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 
 
 class ActivityMovimientos : AppCompatActivity() {
     lateinit var binding: ActivityMovimientosBinding
-
-
-
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMovimientosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var etDates = binding.etdate
+
+        var etDates = binding.textField
+
+        //Calendar Date
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                //.setInputMode(MaterialDatePicker.INPUT_MODE_TEXT)
+                .build()
+
+        etDates.setOnClickListener() {
+
+           datePicker.show(supportFragmentManager, "Material Date Picker")
+            datePicker.addOnPositiveButtonClickListener {
+                etDates.setText(datePicker.headerText)
+            }
+
+        }
+    }
+
+
+
+
+/*
+
         var dpDates = binding.dpFecha
+        var etDates = binding.etdate
 
 
 
@@ -53,7 +77,10 @@ class ActivityMovimientos : AppCompatActivity() {
 
         binding.dpFecha.visibility = View.VISIBLE
     }
+*/
 
 }
+
+
 
 
